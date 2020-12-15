99c523e907883cd89a319c2cfb19d1a669c0812f
Oleksii Perov <oleksii.perov@nure.ua>
2020-12-16 00:23:48 +0200

    initial commit

---
+++ Rendering Engine/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Rendering Engine
+//
+//  Created by AlChWi on 12/16/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Rendering Engine/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Rendering Engine
+//
+//  Created by AlChWi on 12/16/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
